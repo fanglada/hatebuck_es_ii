848529b1017f743ef0db4e5dd0bc3d0375d4a4d3
Ferran <63797008+fanglada@users.noreply.github.com>
2025-01-12 23:14:08 +0100

    afegir relacions i text

--- src/Usuari.java
+++ src/Usuari.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class Usuari {
     private String nomUsuari;
@@ -6,11 +6,17 @@ public class Usuari {
     private String contrasenya;
     private List<Missatge> missatgesEnviats;
     private List<Missatge> missatgesRebuts;
+    private Map<Usuari, EstatUsuari> relacions;
+    private List<Text> texts;
 
-    public Usuari(String nomUsuari, String mail, String contrasenya) {
+    public Usuari(String nomUsuari, String mail, String contrasenya, List<Text> texts) {
         this.nomUsuari = nomUsuari;
         this.mail = mail;
         this.contrasenya = contrasenya;
+        this.missatgesEnviats = new ArrayList<>();
+        this.missatgesRebuts = new ArrayList<>();
+        this.relacions = new HashMap<>();
+        this.texts = texts;
     }
 
     public String getNomUsuari() {
@@ -19,6 +25,18 @@ public class Usuari {
 
     public String getContrasenya() {
         return contrasenya;
+    }
+
+    public void assignarRelacio(Usuari usuari, EstatUsuari estat) {
+
+        if(relacions.containsKey(usuari))
+            relacions.replace(usuari, estat);
+        else
+            relacions.put(usuari, estat);
+    }
+
+    public void eliminarRelacio(Usuari usuari) {
+        relacions.remove(usuari);
     }
 
     public void enviarMissatgePrivat(Usuari destinatari, Missatge missatge, boolean notificar) {
