a2e6835a41b788c24e82338187d40f8191f1a0f4
Àlex <aruizmaso@gmail.com>
2025-01-12 19:55:10 +0100

    Funció login feta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Usuari> usuaris;
+
+        boolean seguirIntentant = true;
+
+        while (login(usuaris)<0 && seguirIntentant) {
+            System.out.println("Vols tornar a intentar-ho? (S/N)");
+            String resposta = System.console().readLine();
+            if(resposta.equals("N")) {
+                seguirIntentant = false;
+            }
+        }
+
+        if(seguirIntentant) {
+            System.out.println("Benvingut!");
+        } else {
+            System.out.println("Adéu!");
+        }
+    }
+
+    //retorna -1 si hi ha un error
+    //retorna 0 si tot ha anat bé
+    private static int inicialitzar(List<Usuari> usuaris)
+    {
+        //emplenar usuaris
+        return 0;
+    }
+
+    //retorna -1 si l'usuari no existeix
+    //retorna -2 si la contrasenya és incorrecta
+    //retorna 1 si tot ha anat bé
+    private static int login(List<Usuari> usuaris) {
+        System.out.println("Entra el nom d'usuari: ");
+        String username = System.console().readLine();
+
+        for (Usuari usuari : usuaris) {
+            if (usuari.getNomUsuari().equals(username)) {
+                System.out.println("Entra la contrasenya: ");
+                String password = System.console().readLine();
+                if (usuari.getContrasenya().equals(password)) {
+                    return 1;
+                }
+                else
+                {
+                    System.out.println("Contrasenya incorrecta");
+                    return -2;
+                }
+            }
+            else
+            {
+                System.out.println("Usuari no trobat");
+                return -1;
+            }
+        }
     }
 }
--- src/Usuari.java
+++ src/Usuari.java
@@ -1,2 +1,19 @@
 public class Usuari {
+    private String nomUsuari;
+    private String mail;
+    private String contrasenya;
+
+    public Usuari(String nomUsuari, String mail, String contrasenya) {
+        this.nomUsuari = nomUsuari;
+        this.mail = mail;
+        this.contrasenya = contrasenya;
+    }
+
+    public String getNomUsuari() {
+        return nomUsuari;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
 }
