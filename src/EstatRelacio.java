6ea75e1a870a3ef8ecf53d5d0ae5b4bd7810e817
Ferran <63797008+fanglada@users.noreply.github.com>
2025-01-12 23:13:35 +0100

    Crear enums

--- src/EstatRelacio.java
+++ src/EstatRelacio.java
@@ -1,2 +1,26 @@
 public enum EstatRelacio {
+
+    AMICS(1),
+    CONEGUTS(2),
+    SALUDATS(3),
+    TOTHOM(4);
+
+    private final int id;
+
+    EstatRelacio(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static EstatRelacio getEstatRelacio(int id) {
+        for (EstatRelacio estatRelacio : EstatRelacio.values()) {
+            if (estatRelacio.getId() == id) {
+                return estatRelacio;
+            }
+        }
+        return null;
+    }
 }
--- src/EstatUsuari.java
+++ src/EstatUsuari.java
@@ -1,2 +1,24 @@
 public enum EstatUsuari {
+    AMIC(1),
+    CONEGUT(2),
+    SALUDAT(3);
+
+    private final int id;
+
+    EstatUsuari(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static EstatUsuari getEstatUsuari(int id) {
+        for (EstatUsuari estatUsuari : EstatUsuari.values()) {
+            if (estatUsuari.getId() == id) {
+                return estatUsuari;
+            }
+        }
+        return null;
+    }
 }
