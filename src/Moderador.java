9af79651e5c26be71b294a93e2c7423bed2a320b
Àlex <aruizmaso@gmail.com>
2025-01-12 21:14:54 +0100

    usuari i moderador

--- src/Moderador.java
+++ src/Moderador.java
@@ -2,8 +2,26 @@ public class Moderador {
 
     String nomUsuari;
     String email;
+
+    String contrasenya;
+
+
     //falten atributs de les relacions
 
+    public Moderador(String nomUsuari, String email, String contrasenya)
+    {
+        this.nomUsuari = nomUsuari;
+        this.email = email;
+        this.contrasenya = contrasenya;
+    }
+
+    public String getNomUsuari() {
+        return nomUsuari;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
     public void eliminarContingut(Text text)
     {
 
--- src/Usuari.java
+++ src/Usuari.java
@@ -1,7 +1,11 @@
+import java.util.List;
+
 public class Usuari {
     private String nomUsuari;
     private String mail;
     private String contrasenya;
+    private List<Missatge> missatgesEnviats;
+    private List<Missatge> missatgesRebuts;
 
     public Usuari(String nomUsuari, String mail, String contrasenya) {
         this.nomUsuari = nomUsuari;
@@ -15,5 +19,11 @@ public class Usuari {
 
     public String getContrasenya() {
         return contrasenya;
+    }
+
+    public void enviarMissatgePrivat(Usuari destinatari, Missatge missatge, boolean notificar) {
+        destinatari.missatgesRebuts.add(missatge);
+        missatgesEnviats.add(missatge);
+
     }
 }
