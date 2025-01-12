a1a801f4dda3415b7643d4879f07af2879e84a9b
Francesc Mayola <francescmayola86@gmail.com>
2025-01-12 21:24:59 +0100

    inicialització

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,99 @@
-import java.sql.SQLOutput;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class Main {
+    static List<Usuari> usuaris = new ArrayList<>();
+    static List<Moderador> moderadors = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<Usuari> usuaris;
-        List<Moderador> moderadors;
 
-        Usuari usuari = new Usuari("","", "");
-
-        boolean seguirIntentant = true;
-
-        while (login(usuaris, usuari)<0 && seguirIntentant) {
-            System.out.println("Vols tornar a intentar-ho? (S/N)");
-            String resposta = System.console().readLine();
-            if(resposta.equals("N")) {
-                seguirIntentant = false;
-            }
+        if(inicialitzar() == -1) {
+            System.out.println("Error inicialitzant la base de dades");
+            return;
         }
 
-        if(seguirIntentant) {
-            String opcio = menu();
-            while (!opcio.equals("0"))
-            {
-                switch (opcio) {
-                case "1":
-                    enviarMissatgePrivat(usuaris,usuari);
-                    break;
-                case "2":
-                    int a = loginModerador(moderadors);
-                    break;
-                case "3":
+//        Usuari usuari = new Usuari("","", "");
+//
+//        boolean seguirIntentant = true;
+//
+//        while (login(usuaris, usuari)<0 && seguirIntentant) {
+//            System.out.println("Vols tornar a intentar-ho? (S/N)");
+//            String resposta = System.console().readLine();
+//            if(resposta.equals("N")) {
+//                seguirIntentant = false;
+//            }
+//        }
+//
+//        if(seguirIntentant) {
+//            String opcio = menu();
+//            while (!opcio.equals("0"))
+//            {
+//                switch (opcio) {
+//                case "1":
+//                    enviarMissatgePrivat(usuaris,usuari);
+//                    break;
+//                case "2":
+//                    int a = loginModerador(moderadors);
+//                    break;
+//                case "3":
+//
+//                    break;
+//                case "0":
+//                    System.out.println("Adéu!");
+//                    break;
+//                default:
+//                    System.out.println("Opció no vàlida");
+//                    break;
+//                }
+//                opcio = menu();
+//            }
+//        } else {
+//            System.out.println("Adéu!");
+//        }
+    }
 
-                    break;
-                case "0":
-                    System.out.println("Adéu!");
-                    break;
-                default:
-                    System.out.println("Opció no vàlida");
-                    break;
-                }
-                opcio = menu();
-            }
-        } else {
-            System.out.println("Adéu!");
+    public static Connection connect() {
+    // connection string
+        String url = "jdbc:sqlite:./db/hatebuck.db";
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+        return conn;
     }
 
     //retorna -1 si hi ha un error
     //retorna 0 si tot ha anat bé
-    private static int inicialitzar(List<Usuari> usuaris, List<Moderador> moderadors)
+    private static int inicialitzar()
     {
-        //emplenar dades
+        Connection conn = connect();
+        ResultSet rs;
+        if (conn == null) {
+            return -1;
+        }
+
+        try {
+            String sql = "SELECT * FROM Usuari";
+            rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                usuaris.add(new Usuari(rs.getString("nomUsuari"), rs.getString("email"), rs.getString("password")));
+            }
+            sql = "SELECT * FROM Moderador";
+            rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                moderadors.add(new Moderador(rs.getString("nomUsuari"), rs.getString("email"), rs.getString("password")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         return 0;
     }
 
@@ -91,30 +134,30 @@ public class Main {
         }
     }
 
-    private static int loginModerador(List<Moderador> moderadors) {
-        System.out.println("Entra el nom d'usuari: ");
-        String username = System.console().readLine();
-
-        for (Moderador moderador : moderadors) {
-            if (moderador.getNomUsuari().equals(username)) {
-                System.out.println("Entra la contrasenya: ");
-                String password = System.console().readLine();
-                if (moderador.getContrasenya().equals(password)) {
-                    return 1;
-                }
-                else
-                {
-                    System.out.println("Contrasenya incorrecta");
-                    return -2;
-                }
-            }
-            else
-            {
-                System.out.println("Usuari no trobat");
-                return -1;
-            }
-        }
-    }
+//    private static int loginModerador(List<Moderador> moderadors) {
+//        System.out.println("Entra el nom d'usuari: ");
+//        String username = System.console().readLine();
+//
+//        for (Moderador moderador : moderadors) {
+//            if (moderador.getNomUsuari().equals(username)) {
+//                System.out.println("Entra la contrasenya: ");
+//                String password = System.console().readLine();
+//                if (moderador.getContrasenya().equals(password)) {
+//                    return 1;
+//                }
+//                else
+//                {
+//                    System.out.println("Contrasenya incorrecta");
+//                    return -2;
+//                }
+//            }
+//            else
+//            {
+//                System.out.println("Usuari no trobat");
+//                return -1;
+//            }
+//        }
+//    }
 
     private static Usuari buscarUsuari(List<Usuari> usuaris, String nomUsuari) {
         for (Usuari usuari : usuaris) {
@@ -125,15 +168,15 @@ public class Main {
         return null;
     }
 
-    private void enviarMissatgePrivat(List<Usuari> usuaris, Usuari usuari)
-    {
-        System.out.println("Entra el nom de l'usuari al que vols enviar el missatge: ");
-        String nomUsuari = System.console().readLine();
-        Usuari destinatari = buscarUsuari(usuaris, nomUsuari);
-
-        Missatge m = entrarMissatge();
-
-        usuari.enviarMissatgePrivat(destinatari, m, false);
-
-    }
+//    private void enviarMissatgePrivat(List<Usuari> usuaris, Usuari usuari)
+//    {
+//        System.out.println("Entra el nom de l'usuari al que vols enviar el missatge: ");
+//        String nomUsuari = System.console().readLine();
+//        Usuari destinatari = buscarUsuari(usuaris, nomUsuari);
+//
+//        Missatge m = entrarMissatge();
+//
+//        usuari.enviarMissatgePrivat(destinatari, m, false);
+//
+//    }
 }
