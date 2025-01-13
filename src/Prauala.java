badf19ff6c770f8fb74c2702b43417724dced754
Ferran <63797008+fanglada@users.noreply.github.com>
2025-01-13 19:55:04 +0100

    Permutacio lletres prauala

--- src/Prauala.java
+++ src/Prauala.java
@@ -3,9 +3,14 @@ import java.util.Random;
 public class Prauala implements Format{
     @Override
     public String contingut(String contingut) {
-        String prauala = contingut;
-        for (int i = 0; i < contingut.length(); i++) {
+        StringBuilder prauala = new StringBuilder(contingut);
+        Random random = new Random();
+        for (int i = 0; i < prauala.length(); i++) {
+            int j = random.nextInt(prauala.length());
+            char temp = prauala.charAt(i);
+            prauala.setCharAt(i, prauala.charAt(j));
+            prauala.setCharAt(j, temp);
         }
-        return prauala;
+        return prauala.toString();
     }
 }
